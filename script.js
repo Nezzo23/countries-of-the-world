37234ad3755fb5c27bedb9beda35562e1838045b
Nemanja Bajakic <nbajakic@gmail.com>
2023-09-15 23:32:00 +0000

    Adding countries and function for answer validation

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-const countries = [
-    "Antigua and Barbuda", "Bahamas", "Barbados", "Belize", "Canada",
-    "Costa Rica", "Cuba", "Dominica", "Dominican Republic", "El Salvador",
-    "Grenada", "Guatemala", "Haiti", "Honduras", "Jamaica", "Mexico",
-    "Nicaragua", "Panama", "Saint Kitts and Nevis", "Saint Lucia",
-    "Saint Vincent and the Grenadines", "Trinidad and Tobago",
-    "United States of America"
-];
+var countryToContinent = {
+    "Antigua and Barbuda": "n-america",
+    "Bahamas": "n-america",
+    "Barbados": "n-america",
+    "Belize": "n-america",
+    "Canada": "n-america",
+
+};
+
+var countries = Object.keys(countryToContinent);
 
 function getRandomCountry() {
     const randomIndex = Math.floor(Math.random() * countries.length);
@@ -13,9 +15,22 @@ function getRandomCountry() {
     return randomCountry;
 }
 
-const randomCountry = getRandomCountry();
-
 document.getElementById('run').addEventListener('click', function () {
     const randomCountry = getRandomCountry();
-    console.log(randomCountry);
+    const correctContinent = countryToContinent[randomCountry];
+
+
+    document.querySelector('.country input').value = randomCountry;
+
+
+    var continents = document.querySelectorAll('.world-map div');
+    continents.forEach(function (continentDiv) {
+        continentDiv.addEventListener('click', function () {
+            if (continentDiv.id === correctContinent) {
+                alert("Correct! The country belongs to " + continentDiv.id);
+            } else {
+                alert("Incorrect. The country does not belong to " + continentDiv.id);
+            }
+        });
+    });
 });
